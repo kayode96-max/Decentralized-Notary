e28a7d23724c278f437f19d85679d14134739284
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-10 19:07:25 +0100

    Update NotaryForm.jsx

--- frontend/src/components/NotaryForm.jsx
+++ frontend/src/components/NotaryForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { sha256 } from 'js-sha256'
 import { showConnect, makeSTXTokenTransfer } from '@stacks/connect'
 
-// NOTE: showConnect / makeSTXTokenTransfer API can vary by version. This code follo
+// NOTE: showConnect / makeSTXTokenTransfer API can vary by version. This code foll
 
 export default function NotaryForm() {
   const [fileName, setFileName] = useState(null)
