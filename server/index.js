a33d25db10bd7c64f57e741f1c9e0d2b63bf7fb0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 16:16:11 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const axios = require('axios')
 const cors = require('cors');
 const app = express();
-app.use(cors())
+app.use(cors()
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const HIRO_BASE = 'https://stacks-blockchain-api.hiro.so;
