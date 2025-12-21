6a7564f24b93ddb3655210da8802d9676238670a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 09:28:59 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="app">
       <header>
         <h1>Decentralized Digital Notary</h1>
-        <p>Hash a file locally and notarize on Stacks (Bitcoin L2).</p>
+        <p>Hash a file locally and notrize on Stacks (Bitcoin L2).</p>
       </header>
 
       <main>
