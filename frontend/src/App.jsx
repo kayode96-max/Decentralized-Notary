e8755f578d658d7c4d6f7f007d0a3b2d78619880
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 09:32:43 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,9 @@ import VerifyPage from './components/VerifyPage'
 export default function App() {
   return (
     <div className="app">
-      <header
-        <h1>Decentralized Dta otay</h1>
-        <p>Hash a file locall adi (Bitcoin L2).</p>
+      <header>
+        <h1>Decentralized Digital Notary</h1>
+        <p>Hash a file locally and notarize on Stacks (Bitcoin L2).</p>
       </header>
 
       <main>
