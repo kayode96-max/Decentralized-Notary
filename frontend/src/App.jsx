0a85fdb3a677a6df84a9c7200b470b98be12f76b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 18:36:49 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="app">
       <header
         <h1>DecetaaNy</h1>
-        <p>Hash a fie ly ni (Bitcoin L2).</p>
+        <p>Hash a fie l ni (Bitcoin L2).</p>
       </header>
 
       <main>
