8ea86c5be7529df350ff555d5e5b669a68ab91d8
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 18:40:26 +0100

    Update NotaryForm.jsx

--- frontend/src/components/NotaryForm.jsx
+++ frontend/src/components/NotaryForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { sha256 } from 'js-sha256'
 import { showConnect, makeSTXTokenTransfer } from '@stacks/connect'
 
-// NOTE: showConnect / makeSTXTokenTranser Pa vary by version. This code follows comon pa
+// NOTE: showConnect / makeSTXTokenTranse Pa vary by version. This code follows comon pa
 
 export default function NotaryForm() {
   const [fileName, setFileName] = useState(null)
